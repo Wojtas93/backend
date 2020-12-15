19dcec9ebc9f3e938ff84f7cca3fd65f6f20d24f
Marek Pedrys <marek.pedrys@gmail.com>
2020-12-15 19:01:06 +0100

    UserController added

--- src/main/java/pl/sdacademy/backend/user/User.java
+++ src/main/java/pl/sdacademy/backend/user/User.java
@@ -63,4 +63,28 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
--- src/main/java/pl/sdacademy/backend/user/UserController.java
+++ src/main/java/pl/sdacademy/backend/user/UserController.java
@@ -1,8 +1,61 @@
 package pl.sdacademy.backend.user;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.sdacademy.backend.guest.Guest;
+import pl.sdacademy.backend.guest.NoSuchGuestException;
 
+import java.util.List;
+import java.util.Optional;
+
+//@CrossOrigin("http://localhost:4200")
 @RestController
+@RequestMapping("/user")
 public class UserController {
+
+    private UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @GetMapping("/{userName}")
+    public User getByName(@PathVariable String userName) {
+        return userRepository.findByUsername(userName).orElse(null);
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public User edit(@RequestBody User sentUser, @PathVariable Long id) {
+
+        Optional<User> userOp = userRepository.findById(id);
+        User user = null;
+        if (userOp.isPresent()) {
+            user = userOp.get();
+        }
+
+        user.setUsername(sentUser.getUsername());
+        user.setPassword(sentUser.getPassword());
+        user.setRole(sentUser.getRole());
+        return userRepository.save(user);
+    }
 
 }
