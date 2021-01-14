aa77264b69db400a373b31db3915c553450ab0b1
Wojciech.Bassara <wojciech.bassara@knowit.no>
2021-01-14 18:02:09 +0100

    added login method

--- src/main/java/pl/sdacademy/backend/user/UserRepository.java
+++ src/main/java/pl/sdacademy/backend/user/UserRepository.java
@@ -6,6 +6,10 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
-    Optional<User> findByFirstNameAndLastName (String firstName, String lastName);
-    Optional<User> findByLastName ( String lastName);
+
+    Optional<User> findByFirstNameAndLastName(String firstName, String lastName);
+
+    Optional<User> findByLastName(String lastName);
+
+    Optional<User> findByUsernameAndPassword(String userName, String encode);
 }
