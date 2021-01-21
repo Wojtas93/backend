b62bea908e7b9815e2457715f178351b6f7574c9
Wojciech Bassara <wojciech.bassara@knowit.no>
2021-01-21 13:13:27 +0100

    csrf enable

--- src/main/java/pl/sdacademy/backend/user/SecurityConfiguration.java
+++ src/main/java/pl/sdacademy/backend/user/SecurityConfiguration.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfAuthenticationStrategy;
+import org.springframework.security.web.csrf.CsrfToken;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
@@ -23,7 +25,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf()
-                .disable()
+//                .disable()
+                .and()
                 .cors()
                 .and()
                 .httpBasic()
