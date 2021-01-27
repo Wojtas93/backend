334a77f53427091f523429da4c22659fbf05bbf3
Wojciech Bassara <wojciech.bassara@knowit.no>
2021-01-27 15:46:11 +0100

    test DB

--- src/main/java/pl/sdacademy/backend/user/NoSuchUserException.java
+++ src/main/java/pl/sdacademy/backend/user/NoSuchUserException.java
@@ -3,9 +3,7 @@ package pl.sdacademy.backend.user;
 import java.util.NoSuchElementException;
 
 public class NoSuchUserException extends NoSuchElementException {
-    public NoSuchUserException(String errorMessage, Throwable err) {
-        super(errorMessage, err);
-    } // potrzebne?
+     // potrzebne?
     public NoSuchUserException(String errorMessage) {
         super(errorMessage);
     }
